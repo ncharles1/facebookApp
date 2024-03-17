2e0a6320661f27ddd36ebcfa7c58d43705f040db
rnfor-pro <nfor.rudolph1@gmail.com>
2024-03-17 15:57:43 -0400

    test script modified

--- test.sh
+++ test.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-echo "We are doing branching in git"
-...
-The power of github.
+echo -n " please enter the directory name: "
+read directory
+sudo mkdir $directory
+git init $(pwd)/$directory
+echo " use your command line terminal to access $directory directory"
+echo "And make sure it a git repository to test the script"
+ls -a $directory | grep .git #this line helps you to detect a git repository special file
+echo " make sure you see the .git file above to confirm that you actually created the repo "
+echo " This is a property of Etech ConsultingDevopsBooster course!!! "
+
